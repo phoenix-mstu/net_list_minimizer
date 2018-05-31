6b988379569d4a5a708c2f3fd80f18b9ad7958c7
Grigory Kuzovnikov <g.kuzovnikov@corp.badoo.com>
2018-05-31 10:00:43 +0300

    New algoritm

--- minimize_net_list.py
+++ minimize_net_list.py
@@ -6,16 +6,19 @@ import re
 import struct
 
 if len(sys.argv) < 3:
-    print('You must give me a filename and percentage')
+    print('You must give me a filename and required list size')
     sys.exit()
 
 filename = str(sys.argv[1])
-percentage = int(sys.argv[2])
+required_list_size = int(sys.argv[2])
 
 file = open(filename, "r")
 if not file:
     print('Cant open file')
     sys.exit()
+
+# Filling the binary tree
+# Each node has maximum two children (by design)
 
 Root = net_tree.Node(net_tree.Net(0,0), 0)
 for line in file.readlines():
@@ -31,9 +34,40 @@ for line in file.readlines():
             mask_size = 32
 
         Root.addSubnet(net_tree.Node(net_tree.Net(ip, mask_size), 1))
-#         Root.printTree(0);
 
+# Updating real ip counters in each node
 
-Root.updateRealIpVolume();
-Root.printMinimizedList(percentage);
+Root.finishTree();
 
+# Making a flat list of fake nodes, in this list each node has a weight atribute.
+# Nodes width less fake ip volume will have lower weight
+
+nodesByFakeIpVolume = []
+Root.getNodesByFakeIpVolume(nodesByFakeIpVolume)
+
+# Sort nodes in list by weight.
+# So collapsing the first nodes in list will have less impact on count of "not real" ips in script result
+# with a greater impact on reducing resulting ips list size
+
+nodesByFakeIpVolume.sort(key=lambda x: x[0])
+
+# Collapsing the nodes untill we have a desired resulting list size
+
+list_size = Root.real_ip_records_count
+for nodeArr in nodesByFakeIpVolume:
+    Node = nodeArr[1]
+    if Node.is_collapsed: continue
+
+    # Each fake node has several child ips that are returned in a resulting list
+    # After collapsing the node resulting list size will be reduced on count of the ips that were collapsed
+    # And increased by 1 - all these ips will be replaced by a single ip
+    list_size = list_size + 1 - Node.recursiveCollapse()
+
+    if list_size <= required_list_size: break
+
+# printing the result
+Root.printCollapsedTree();
+
+# printing some stats
+print('### list size:    ' + str(list_size))
+print('### not real ips: ' + str(Root.getNotRealIpCount()))
--- net_tree.py
+++ net_tree.py
@@ -4,35 +4,27 @@ BIG_MASK = (1 << 32) - 1
 def getMaskByMaskSize(mask_size):
     return BIG_MASK ^ ((1 << (32 - mask_size)) - 1)
 
+def getIpVolumeByMaskSize(mask_size):
+    return 1 << (32 - mask_size);
+
 class Net:
     def __init__(self, net: int, mask_size: int):
         self.mask_size = mask_size
         self.net = net & getMaskByMaskSize(mask_size)
         self.mask = getMaskByMaskSize(self.mask_size)
-
-    def getNet(self):
-        return self.net
-
-    def getMaskSize(self):
-        return self.mask_size
-
-    def getMask(self):
-        return getMaskByMaskSize(self.mask_size)
-
-    def getIpVolume(self):
-        return 1 << (32 - self.mask_size);
+        self.ip_volume = getIpVolumeByMaskSize(mask_size)
 
     def hasSubnet(self, Net: 'Net'):
         if Net.mask_size <= self.mask_size: return 0
         return self.net == Net.net & self.mask
 
     def isSameNet(self, Net: 'Net'):
-        return Net.mask_size == self.mask_size and Net.net == self.net
+        return (Net.mask_size == self.mask_size) and (Net.net == self.net)
 
     def getCommonNet(self, OtherNet: 'Net', min_mask_size: int):
         if self.mask_size <= min_mask_size: return 0
         if OtherNet.mask_size <= min_mask_size: return 0
-        for mask_size in range(min(self.mask_size, OtherNet.mask_size) - 1, min_mask_size, -1):
+        for mask_size in range(min(self.mask_size, OtherNet.mask_size) - 1, min_mask_size - 1, -1):
           mask = getMaskByMaskSize(mask_size)
           if (self.net & mask) == (OtherNet.net & mask):
               return Net(self.net, mask_size)
@@ -48,19 +40,22 @@ class Net:
         return '.'.join(reversed(bytes)) + ('' if self.mask_size == 32 else "/" + str(self.mask_size))
 
 class Node:
-    def __init__(self, net: Net, is_real_net: bool):
+    def __init__(self, net: Net, is_real_net: int):
         self.net = net
         self.children = []
         self.is_real_net = is_real_net
-        self.real_ip_volume = 0;
+        self.real_ip_volume = 0
+        self.is_collapsed = 0
+        self.real_ip_records_count = 0
 
     def getNet(self):
         return self.net
 
     def addSubnet(self, NewNode: 'Node'):
-        if not self.is_real_net and NewNode.is_real_net and self.net.isSameNet(NewNode.net):
-            self.is_real_net = 1
-            self.children = []
+        if self.net.isSameNet(NewNode.net):
+            if not self.is_real_net and NewNode.is_real_net:
+                self.is_real_net = 1
+                self.children = []
             return 1
 
         if self.is_real_net and self.net.hasSubnet(NewNode.net):
@@ -86,25 +81,59 @@ class Node:
         return 1
 
     def printTree(self, level):
-        str = ''
+        prefix = ''
         for i in range(level):
-            str = str + '    '
-        print(str + self.net.getAsString())
+            prefix = prefix + '    '
+
+        if self.is_real_net: sign = '*'
+        elif self.is_collapsed: sign = '.'
+        else: sign = ''
+
+        print(prefix + self.net.getAsString() + ' ' + sign)
         for i, Child in enumerate(self.children):
             Child.printTree(level + 1)
 
-    def updateRealIpVolume(self):
+    def finishTree(self):
         if self.is_real_net:
-            self.real_ip_volume = self.net.getIpVolume();
+            self.real_ip_volume = self.net.ip_volume
+            self.real_ip_records_count = 1
         else:
+            self.real_ip_volume = 0
+            self.real_ip_records_count = 0
             for Child in self.children:
-                self.real_ip_volume += Child.updateRealIpVolume();
-        return self.real_ip_volume;
+                Child.finishTree()
+                self.real_ip_volume += Child.real_ip_volume
+                self.real_ip_records_count += Child.real_ip_records_count
 
-    def printMinimizedList(self, percentage):
-        current_percentage = 100 if self.is_real_net else 100 * self.real_ip_volume / self.net.getIpVolume()
-        if (current_percentage >= percentage):
-            print(self.net.getAsString() + ' ' + str(current_percentage))
+    def printCollapsedTree(self):
+        if self.is_real_net or self.is_collapsed:
+            print(self.net.getAsString() + ' ' + str(self.net.ip_volume - self.real_ip_volume))
         else:
             for Child in self.children:
-                Child.printMinimizedList(percentage)
+                Child.printCollapsedTree()
+
+    def getNodesByFakeIpVolume(self, list):
+        sort_key = (self.net.ip_volume - self.real_ip_volume) * 100 + self.net.mask_size
+        list.append([sort_key, self])
+        for Child in self.children:
+            if not Child.is_real_net:
+                Child.getNodesByFakeIpVolume(list)
+
+    def recursiveCollapse(self):
+        if self.is_collapsed or self.is_real_net: return 1
+        self.is_collapsed = 1
+        networks_count = 0
+        for Child in self.children:
+            networks_count = networks_count + Child.recursiveCollapse()
+        return networks_count
+
+    def getNotRealIpCount(self):
+        if self.is_real_net: return 0
+        if self.is_collapsed: return self.net.ip_volume - self.real_ip_volume
+        res = 0
+        for Child in self.children:
+            res = res + Child.getNotRealIpCount()
+        return res
+
+
+
